a1a771353f4ea00c1b370de59bcc4d23071af201
Daniela Faria <danii.faria2021@gmail.com>
2023-11-20 15:13:23 -0300

    Testes

---
+++ teste/TesteObserver.py
@@ -0,0 +1,40 @@
+import unittest
+from io import StringIO
+from unittest.mock import patch
+from scr.Observador import TotalWordObserver, EvenLengthWordObserver, CaptalizedWordObserver
+from scr.Observavel import WordCounter
+
+class TestWordCounter(unittest.TestCase):
+    def setUp(self):
+        self.word_counter = WordCounter()
+
+    def test_total_word_observer(self):
+        total_word_observer = TotalWordObserver()
+        self.word_counter.add_observer(total_word_observer)
+
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            self.word_counter.process_input("Isso é um teste")
+            expected_output = "Total de palavras: 4\n"
+            self.assertEqual(mock_stdout.getvalue(), expected_output)
+
+    def test_even_length_word_observer(self):
+        even_length_word_observer = EvenLengthWordObserver()
+        self.word_counter.add_observer(even_length_word_observer)
+
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            self.word_counter.process_input("Palavras com tamanhos pares")
+            expected_output = "Palavras com quantidade par de caracteres: 2\n"
+            self.assertEqual(mock_stdout.getvalue(), expected_output)
+
+    def test_capitalized_word_observer(self):
+        capitalized_word_observer = CaptalizedWordObserver()
+        self.word_counter.add_observer(capitalized_word_observer)
+
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            self.word_counter.process_input("Começadas com Maiúsculas")
+            expected_output = "Palavras começadas com maiúsculas: 2\n"
+            self.assertEqual(mock_stdout.getvalue(), expected_output)
+
+
+if _name_ == '_main_':
+    unittest.main()
